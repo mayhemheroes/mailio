e0ac37b23554014cbe096eeace6f0f3fb57dbc64
karas <contact@alepho.com>
2022-11-03 21:41:23 +0100

    Removed character literals.

--- src/message.cpp
+++ src/message.cpp
@@ -1539,7 +1539,8 @@ string_t message::parse_address_name(const string& address_name) const
                 encoding = std::get<1>(an);
                 decoded += std::get<0>(an);
                 start = end + codec::ENCODING_END.length();
-                while (start < addr_len && (address_name[start] == ' ' || address_name[start] == '\r' || address_name[start] == '\n'))
+                while (start < addr_len &&
+                    (address_name[start] == codec::SPACE_CHAR || address_name[start] == codec::CR_CHAR || address_name[start] == codec::LF_CHAR))
                 {
                     start++;
                 }
